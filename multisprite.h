f7fe064398e1ae3c8aba2343188981c7938ad8a5
chaoh <chaoh@g.clemson.edu>
2014-10-17 09:21:14 +0800

    first commit

---
+++ multisprite.h
@@ -0,0 +1,41 @@
+#ifndef MULTISPRITE__H
+#define MULTISPRITE__H
+#include <string>
+#include <vector>
+#include "drawable.h"
+
+class MultiSprite : public Drawable {
+public:
+  MultiSprite(const std::string&);
+  MultiSprite(const MultiSprite&);
+  virtual ~MultiSprite() { } 
+
+  virtual void draw() const;
+  virtual void update(Uint32 ticks);
+  virtual const Frame* getFrame() const { 
+    return frames[currentFrame]; 
+  }
+
+  const bool getDirection();
+  void setDirection(bool);
+
+  const std::string getFrameName();
+
+protected:
+  std::vector<Frame *> frames;
+  int worldWidth;
+  int worldHeight;
+
+  unsigned currentFrame;
+  unsigned numberOfFrames;
+  unsigned frameInterval;
+  float timeSinceLastFrame;
+  int frameWidth;
+  int frameHeight;
+
+  bool frameDirection;
+  std::string frameName;
+
+  void advanceFrame(Uint32 ticks);
+};
+#endif
---
+++ sprite.h
@@ -0,0 +1,38 @@
+#ifndef SPRITE__H
+#define SPRITE__H
+#include <string>
+#include "drawable.h"
+
+class Sprite : public Drawable {
+public:
+  Sprite(const std::string&);
+  Sprite(const std::string&, const Vector2f& pos, const Vector2f& vel);
+  Sprite(const std::string&, 
+         const Vector2f& pos, const Vector2f& vel, const Frame*);
+  Sprite(const Sprite& s);
+  virtual ~Sprite() { } 
+  Sprite& operator=(const Sprite&);
+
+  virtual const Frame* getFrame() const { return frame; }
+  virtual void draw() const;
+
+  virtual void update(Uint32 ticks);
+  const bool getCatched(){
+    return catched;
+  }
+  void setCatched(const bool x){
+    catched = x;
+  }
+
+private:
+  const Frame * frame;
+  int frameWidth;
+  int frameHeight;
+  int worldWidth;
+  int worldHeight;
+
+  bool catched;
+
+  int getDistance(const Sprite*) const;
+};
+#endif
